074db051580f96cfc67848aea3a918a867d17784
RahifM <faizel326@gmail.com>
2017-09-09 06:33:20 +0000

    media: that aint needed
    Change-Id: If5d136c9bbc251b8e039564526c0e74466c300a1
    Signed-off-by: RahifM <faizel326@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,9 +18,9 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     include $(QCOM_MEDIA_ROOT)/mm-video-legacy/Android.mk
   endif
 
-  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
-    include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
-  endif
+#  ifneq ($(filter msm8610 msm8226 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+ #   include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+ # endif
 
   ifneq ($(filter msm8610 msm8226 msm8960 msm8084 msm8952 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
     include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
