xyyx/android_hardware_qcom_media-caf-msm8996

bring back TARGET_QCOM_MEDIA_VARIANT

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -12,4 +13,4 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
 include $(QCOM_MEDIA_ROOT)/videopp/Android.mk
 endif
-
+endif

conf_files/config.mk
@@ -1,5 +1,5 @@
 ifeq ($(TARGET_BOARD_PLATFORM),msm8937)
-PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msm8937/media_profiles_8937.xml:system/etc/media_profiles.xml \
-                      hardware/qcom/media/conf_files/msm8937/media_codecs_8937.xml:system/etc/media_codecs.xml \
-                      hardware/qcom/media/conf_files/msm8937/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml
+PRODUCT_COPY_FILES += hardware/qcom/media-caf-msm8996/conf_files/msm8937/media_profiles_8937.xml:system/etc/media_profiles.xml \
+                      hardware/qcom/media-caf-msm8996/conf_files/msm8937/media_codecs_8937.xml:system/etc/media_codecs.xml \
+                      hardware/qcom/media-caf-msm8996/conf_files/msm8937/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml
 endif #TARGET_BOARD_PLATFORM

mm-video-v4l2/vidc/common/Android.mk
@@ -21,9 +21,9 @@ libmm-vidc-def += -D_ANDROID_ICS_
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-msm8996/mm-core/inc
 libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-msm8996/libc2dcolorconvert
 libmm-vidc-inc      += $(TOP)/frameworks/av/include/media/stagefright
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 

mm-video-v4l2/vidc/vdec/Android.mk
@@ -66,13 +66,13 @@ include $(CLEAR_VARS)
 
 # Common Includes
 libmm-vdec-inc          := $(LOCAL_PATH)/inc
-libmm-vdec-inc          += $(TOP)/hardware/qcom/media/mm-video-v4l2/vidc/common/inc
-libmm-vdec-inc          += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vdec-inc          += $(TOP)/hardware/qcom/media-caf-msm8996/mm-video-v4l2/vidc/common/inc
+libmm-vdec-inc          += $(TOP)/hardware/qcom/media-caf-msm8996/mm-core/inc
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/openmax
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/hardware
-libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media-caf-msm8996/libc2dcolorconvert
 libmm-vdec-inc      	+= $(TOP)/frameworks/av/include/media/stagefright
 libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/SwVdec
 libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/swvdec
@@ -80,7 +80,7 @@ libmm-vdec-inc      	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 ifeq ($(PLATFORM_SDK_VERSION), 18)  #JB_MR2
 libmm-vdec-def += -DANDROID_JELLYBEAN_MR2=1
-libmm-vdec-inc += $(TOP)/hardware/qcom/media/libstagefrighthw
+libmm-vdec-inc += $(TOP)/hardware/qcom/media-caf-msm8996/libstagefrighthw
 endif
 
 # Common Dependencies

mm-video-v4l2/vidc/venc/Android.mk
@@ -69,14 +69,14 @@ endif
 
 # Common Includes
 libmm-venc-inc      := $(LOCAL_PATH)/inc
-libmm-venc-inc      += $(TOP)/hardware/qcom/media/mm-video-v4l2/vidc/common/inc
-libmm-venc-inc      += hardware/qcom/media/mm-core/inc
-libmm-venc-inc      += hardware/qcom/media/libstagefrighthw
+libmm-venc-inc      += $(TOP)/hardware/qcom/media-caf-msm8996/mm-video-v4l2/vidc/common/inc
+libmm-venc-inc      += hardware/qcom/media-caf-msm8996/mm-core/inc
+libmm-venc-inc      += hardware/qcom/media-caf-msm8996/libstagefrighthw
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/adreno
 libmm-venc-inc      += frameworks/native/include/media/hardware
 libmm-venc-inc      += frameworks/native/include/media/openmax
-libmm-venc-inc      += hardware/qcom/media/libc2dcolorconvert
+libmm-venc-inc      += hardware/qcom/media-caf-msm8996/libc2dcolorconvert
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/libvqzip
 libmm-venc-inc      += frameworks/av/include/media/stagefright
 libmm-venc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include

RahifM/hardware_qcom_media

Signed-off-by: RahifM <faizel326@gmail.com>

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8937)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk

conf_files/config.mk
@@ -1,5 +1,5 @@
 ifeq ($(TARGET_BOARD_PLATFORM),msm8937)
-PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msm8937/media_profiles_8937.xml:system/etc/media_profiles.xml \
-                      hardware/qcom/media/conf_files/msm8937/media_codecs_8937.xml:system/etc/media_codecs.xml \
-                      hardware/qcom/media/conf_files/msm8937/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml
+PRODUCT_COPY_FILES += hardware/qcom/media-caf-msm8937/conf_files/msm8937/media_profiles_8937.xml:system/etc/media_profiles.xml \
+                      hardware/qcom/media-caf-msm8937/conf_files/msm8937/media_codecs_8937.xml:system/etc/media_codecs.xml \
+                      hardware/qcom/media-caf-msm8937/conf_files/msm8937/media_codecs_performance_8937.xml:system/etc/media_codecs_performance.xml
 endif #TARGET_BOARD_PLATFORM

mm-video-v4l2/vidc/common/Android.mk
@@ -21,9 +21,9 @@ libmm-vidc-def += -D_ANDROID_ICS_
 # ---------------------------------------------------------------------------------
 
 libmm-vidc-inc      := $(LOCAL_PATH)/inc
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-msm8937/mm-core/inc
 libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
-libmm-vidc-inc      += $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vidc-inc      += $(TOP)/hardware/qcom/media-caf-msm8937/libc2dcolorconvert
 libmm-vidc-inc      += $(TOP)/frameworks/av/include/media/stagefright
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 

mm-video-v4l2/vidc/vdec/Android.mk
@@ -64,13 +64,13 @@ include $(CLEAR_VARS)
 
 # Common Includes
 libmm-vdec-inc          := $(LOCAL_PATH)/inc
-libmm-vdec-inc          += $(TOP)/hardware/qcom/media/mm-video-v4l2/vidc/common/inc
-libmm-vdec-inc          += $(TOP)/hardware/qcom/media/mm-core/inc
+libmm-vdec-inc          += $(TOP)/hardware/qcom/media-caf-msm8937/mm-video-v4l2/vidc/common/inc
+libmm-vdec-inc          += $(TOP)/hardware/qcom/media-caf-msm8937/mm-core/inc
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/openmax
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/hardware
-libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media/libc2dcolorconvert
+libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media-caf-msm8937/libc2dcolorconvert
 libmm-vdec-inc      	+= $(TOP)/frameworks/av/include/media/stagefright
 libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/SwVdec
 libmm-vdec-inc      	+= $(TARGET_OUT_HEADERS)/mm-video/swvdec
@@ -78,7 +78,7 @@ libmm-vdec-inc      	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 ifeq ($(PLATFORM_SDK_VERSION), 18)  #JB_MR2
 libmm-vdec-def += -DANDROID_JELLYBEAN_MR2=1
-libmm-vdec-inc += $(TOP)/hardware/qcom/media/libstagefrighthw
+libmm-vdec-inc += $(TOP)/hardware/qcom/media-caf-msm8937/libstagefrighthw
 endif
 
 # Common Dependencies

mm-video-v4l2/vidc/venc/Android.mk
@@ -76,14 +76,14 @@ endif
 
 # Common Includes
 libmm-venc-inc      := $(LOCAL_PATH)/inc
-libmm-venc-inc      += $(TOP)/hardware/qcom/media/mm-video-v4l2/vidc/common/inc
-libmm-venc-inc      += hardware/qcom/media/mm-core/inc
-libmm-venc-inc      += hardware/qcom/media/libstagefrighthw
+libmm-venc-inc      += $(TOP)/hardware/qcom/media-caf-msm8937/mm-video-v4l2/vidc/common/inc
+libmm-venc-inc      += hardware/qcom/media-caf-msm8937/mm-core/inc
+libmm-venc-inc      += hardware/qcom/media-caf-msm8937/libstagefrighthw
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/adreno
 libmm-venc-inc      += frameworks/native/include/media/hardware
 libmm-venc-inc      += frameworks/native/include/media/openmax
-libmm-venc-inc      += hardware/qcom/media/libc2dcolorconvert
+libmm-venc-inc      += hardware/qcom/media-caf-msm8937/libc2dcolorconvert
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/libvqzip
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/libgpustats
 libmm-venc-inc      += frameworks/av/include/media/stagefright