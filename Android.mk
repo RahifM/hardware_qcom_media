324aa7d57161d26df6083f2d2aac5dea07262922
RahifM <faizel326@gmail.com>
2021-05-27 18:38:58 +0530

    Revert "Add TARGET_QCOM_MEDIA_VARIANT"
    This reverts commit 8badace8c28363245cbdbc75b85e5d910a95180f.
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list
+#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
@@ -21,5 +20,4 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
-endif
 endif
